4c58989cc6ce439fa2294cc5422417e3546bb401
dmiweb <dimbons@mail.ru>
2024-10-30 21:51:18 +0300

    upd fetch url  createRequest.js

--- src/js/components/api/createRequest.js
+++ src/js/components/api/createRequest.js
@@ -1,5 +1,5 @@
 const createRequest = async (options = {}) => {
-  return await fetch("http://localhost:3000/news", options);
+  return await fetch("https://loading-styling-backend-80ii1p8gm-dmiwebs-projects.vercel.app/news", options);
 };
 
 export default createRequest;
