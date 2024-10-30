a40ef89c226b51195c9de04aa56415be5597ce20
dmiweb <dimbons@mail.ru>
2024-10-30 22:04:29 +0300

    upd files

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## (on Service Worker)
 
-**GitHub Page** - 
+**GitHub Page** - https://dmiweb.github.io/loading-styling/
 
 [![Build status](https://ci.appveyor.com/api/projects/status/n2162vwk0qo5lkpg?svg=true)](https://ci.appveyor.com/project/dmiweb/loading-styling)
--- src/js/components/api/createRequest.js
+++ src/js/components/api/createRequest.js
@@ -1,5 +1,5 @@
 const createRequest = async (options = {}) => {
-  return await fetch("https://loading-styling-backend-80ii1p8gm-dmiwebs-projects.vercel.app/news", options);
+  return await fetch("https://loading-styling-backend.vercel.app/news", options);
 };
 
 export default createRequest;
